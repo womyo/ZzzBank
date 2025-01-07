815dd64ff871cecfef4a68a701fa40e53e4cec86
Lee Inho <inhoking01@gmail.com>
2025-01-07 23:34:37 +0900

    feat. add tired rate with gaugeView

---
+++ ZzzBank/GaugeView.swift
@@ -0,0 +1,39 @@
+//
+//  GaugeView.swift
+//  ZzzBank
+//
+//  Created by 이인호 on 1/7/25.
+//
+
+import SwiftUI
+
+struct GaugeView: View {
+    @State private var current = 67.0
+    @State private var minValue = 0.0
+    @State private var maxValue = 100.0
+    let gradient = Gradient(colors: [.green, .yellow, .orange, .red])
+    
+    
+    var body: some View {
+        Gauge(value: current, in: minValue...maxValue) {
+            Image(systemName: "heart.fill")
+                .foregroundColor(.red)
+        } currentValueLabel: {
+            Text("예상 피로도")
+                .foregroundColor(Color.green)
+        } minimumValueLabel: {
+            Text("L")
+                .foregroundColor(Color.green)
+        } maximumValueLabel: {
+            Text("H")
+                .foregroundColor(Color.red)
+        }
+        .gaugeStyle(.accessoryCircular)
+        .tint(gradient)
+        .scaleEffect(1.2)
+    }
+}
+
+#Preview {
+    GaugeView()
+}
