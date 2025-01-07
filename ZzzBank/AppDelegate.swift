def0866e7e5e61baf2062619495e88813bbe456f
Lee Inho <inhoking01@gmail.com>
2025-01-07 21:28:14 +0900

    feat. add healthKit

--- ZzzBank/AppDelegate.swift
+++ ZzzBank/AppDelegate.swift
@@ -10,10 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let healthKitService = HealthKitService()
+        healthKitService.configure()
+        
         return true
     }
 
