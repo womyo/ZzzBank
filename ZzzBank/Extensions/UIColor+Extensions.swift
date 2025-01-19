bdbfa0feddb86824db9d88d41154d4c9e59da221
Lee Inho <inhoking01@gmail.com>
2025-01-19 23:16:07 +0900

    feat. add colorsets

---
+++ ZzzBank/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extensions.swift
+//  ZzzBank
+//
+//  Created by 이인호 on 1/19/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static let customBackgroundColor: UIColor = UIColor(named: "customBackgroundColor")!
+    static let shadowColor: UIColor = UIColor(named: "shadowColor")!
+}
---
+++ ZzzBank/Extensions/UIView+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+Extension.swift
+//  ZzzBank
+//
+//  Created by 이인호 on 1/19/25.
+//
+
+import UIKit
+
+extension UIView {
+    func setViewShadow(backView: UIView) {
+        layer.masksToBounds = true
+        layer.cornerRadius = 20
+        
+        backView.layer.masksToBounds = false
+        backView.layer.shadowOpacity = 0.7
+        backView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        backView.layer.shadowRadius = 5
+        backView.layer.shadowColor = UIColor.shadowColor.cgColor
+    }
+}
