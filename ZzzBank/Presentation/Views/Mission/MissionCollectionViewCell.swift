22ac5c48303daba611dca37b4d49d59ea6f8cdab
Inho Lee <harrylee0116@gmail.com>
2025-04-10 17:38:47 +0900

    feat. add mission bingo view

---
+++ ZzzBank/Presentation/Views/Mission/MissionCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MissionCollectionViewCell.swift
+//  ZzzBank
+//
+//  Created by wayblemac02 on 4/10/25.
+//
+
+import UIKit
+
+class MissionCollectionViewCell: UICollectionViewCell {
+    static let identifier = "MissionCollectionViewCell"
+}
