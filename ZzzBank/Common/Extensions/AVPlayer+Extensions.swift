e320f11563e4d11983ea95e2d2c6eb0f95ade7c8
Inho Lee <harrylee0116@gmail.com>
2025-04-21 15:53:10 +0900

    feat. add extension for AVPlayer

--- ZzzBank/Common/Extensions/AVPlayer+Extensions.swift
+++ ZzzBank/Common/Extensions/AVPlayer+Extensions.swift
@@ -5,4 +5,10 @@
 //  Created by wayblemac02 on 4/21/25.
 //
 
-import Foundation
+import AVFoundation
+
+extension AVPlayer{ // 영상이 현재 진행중인지 판단하는 부분
+    var isPlaying: Bool {
+        return rate != 0 && error == nil
+    }
+}
